b47cf10c5dd656df5848cf4d5267e9dae9617483
mariiapasko <mariia.pasko85@gmail.com>
2023-02-11 23:23:39 +0100

    Add GitHub repository link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       Weather app
+      <footer>
+        This project is coded by Mariia Pasko and {" "}
+        <a href="https://github.com/mariiapasko/react-weather-app" target="_blank"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
