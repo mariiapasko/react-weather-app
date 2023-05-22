89538d3dcefbc77c4cbb19d251a5f8ae611a0229
mariiapasko <mariia.pasko85@gmail.com>
2023-05-22 20:21:36 +0200

    Changed netlify link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       <Weather  defaultCity="Kiev" />
       <footer>
         <a href="https://github.com/mariiapasko/react-weather-app" target="_blank" rel="noopener noreferrer"> Open-sourced on GitHub</a> by Mariia Pasko and hosted on
-        <a href="" target ="_blank" rel="noopener noreferrer"> Netlify </a>
+        <a href="https://resplendent-tartufo-d1fd33.netlify.app/" target ="_blank" rel="noopener noreferrer"> Netlify </a>
       </footer>
       </div>
     </div>
